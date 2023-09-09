ad42e4b33a5101d3b91cb265a2992fe155ccd9dc
Tiago-salva <maitetiago58@gmail.com>
2023-09-09 15:50:25 -0300

    Added instructions to understand it

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ const gameBoard = (() => {
 
 })();
 
-//Function Facotry
+//Function Facotry for the player
 const createPlayer = (name, symbol) => {
     return { name, symbol };
 };
 
-//Module
+//Module for the game
 function gameController() {
     const playerOne = createPlayer("playerOne", "X");
     const playerTwo = createPlayer("playerTwo", "O");
@@ -58,6 +58,7 @@ function gameController() {
         return
     }
 
+    //Function to get the best move
     const minimax = (board, depth, isMaximazing) => { 
         if (checkWhichPlayerWin(board, playerOne.symbol)) {
             return -100
@@ -128,7 +129,7 @@ function gameController() {
         if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[0][2] !== '') {
             return true
         }
-
+        //If no winner, return false
         return false
     }
 
@@ -137,14 +138,14 @@ function gameController() {
             for (let row = 0; row < 3; row++) {
                 for (let col = 0; col < 3; col++) {
                     if (board[row][col] === "") {
-                        return false; // Encontró una celda vacía, el tablero no está lleno
+                        return false; // Found an empty cell, the board it's not full
                     }
                 }
             }
-            return true; // No se encontraron celdas vacías, el tablero está lleno
+            return true; // There is not empty cells, the board it's full
         }
         
-        
+        //Check for a draw / tie
         if (isBoardFull(board) && (checkWinner(board) === false)) {
             return true
         } else {
@@ -152,6 +153,7 @@ function gameController() {
         }
     }
 
+    //Function to check which player won
     function checkWhichPlayerWin(board, symbol) {
         let thePlayerWin = false;
         
@@ -181,6 +183,7 @@ function gameController() {
         return thePlayerWin
     }
 
+    //Reset the board
     const restartBoard = () => {
         const board = gameBoard.getBoard();
         currentPlayer = playerOne;
@@ -196,39 +199,46 @@ function gameController() {
 };
 
 
-//Module
+//Module to control the screen
 const screenController = ((gameController) => {
 
     let opponentOption;
+    //Get visual elements
     const whatDifficultyText = document.querySelector(".what-difficulty");
     const opponentBtns = document.querySelectorAll("#opponent-btn");
     const chooseOpponentContainer = document.querySelector(".choose-opponent-container");
+    //Get who is the opponent
     opponentBtns.forEach((button) => {
         button.addEventListener("click", () => {
             opponentOption = button.getAttribute("data-attribute");
             opponentOption = parseInt(opponentOption);
             chooseOpponentContainer.style.visibility = "hidden";
+            //If the opponent it's IA, show the difficulty text
             if (opponentOption === 2) {
                 whatDifficultyText.style.visibility = "visible";
             }
             return opponentOption
         })
     })
-
+    
+    //Buttno to restart the game
     const restartBtn = document.getElementById("restart-btn");
     restartBtn.addEventListener("click", () => {
         gameController.restartBoard();
         updateBoardUI();
     });
 
+    //Event Listener to show the three difficultys
     const showDifficultyBtn = document.getElementById("select-difficulty-btn");
     const difficultyBtnContainer = document.querySelector(".choose-difficulty");
     showDifficultyBtn.addEventListener("click", () => {
         difficultyBtnContainer.classList.toggle("showed");
     });
 
+    //Default difficulty
     let difficultyNumber = 2;
     
+    //When a difficulty it's selected, the text it's updated and the board restarted
     const difficultyBtns = document.querySelectorAll("#difficulty-btn");
     difficultyBtns.forEach((button) => {
         button.addEventListener("click", () => {
@@ -243,7 +253,9 @@ const screenController = ((gameController) => {
         })
     })
 
+    //Function to show who won
     const showWinner = () => {
+        //Get the neccessary elements
         const blur = document.querySelector(".blur");
         const showWinnerContainer = blur.querySelector(".show-winner");
         const winnerTitle = blur.querySelector(".winner-title");
@@ -252,6 +264,7 @@ const screenController = ((gameController) => {
         blur.classList.add("display");
         showWinnerContainer.classList.add("active");
 
+        //When the overlay it's clicked, remove it and restart the game
         blur.addEventListener("click", () => {
             blur.classList.remove("display");
             showWinnerContainer.classList.remove("active");
@@ -261,9 +274,11 @@ const screenController = ((gameController) => {
             updateBoardUI();
         });
 
+        //Save in variables the return of the functions
         const checkResult = gameController.checkWinner(gameBoard.getBoard());
         const isDraw = gameController.checkDraw(gameBoard.getBoard());
 
+        //Check if there is a winner or a tie
         if (checkResult === true) {
             winnerTitle.textContent = "The winner is";
             winnerSymbol.textContent = gameController.getCurrentPlayer().symbol;
@@ -273,6 +288,7 @@ const screenController = ((gameController) => {
         }
     }
 
+    //Function to update the board
     const updateBoardUI = () => {
         const board = gameBoard.getBoard();
         const allCells = document.querySelectorAll(".cell");
@@ -284,6 +300,7 @@ const screenController = ((gameController) => {
         });
     };
 
+    //Function that control all the game
     const playRound = () => {
         const board = gameBoard.getBoard();
 
@@ -291,26 +308,31 @@ const screenController = ((gameController) => {
 
         const allCells = document.querySelectorAll(".cell");
 
+        //Event Listener for all the cells
         allCells.forEach(cell => {
             cell.addEventListener("click", () => {
                 let someoneWin, isDraw;
 
+                //Infinite loop until someone win or there it's a tie
                 while (true) {
                     
+                    //Get the number of col and row of the cell
                     rowIndex = cell.getAttribute("data-row");
                     colIndex = cell.getAttribute("data-col");
     
+                    //Get the symbol of the actual player
                     let symbol = gameController.getCurrentPlayer().symbol;
     
+                    //If the cell it's full, try another move
                     if (board[rowIndex][colIndex] !== "") {
                         return
                     }
     
                     gameBoard.makeMove(rowIndex, colIndex, symbol);
                     updateBoardUI();
+                    //Check if the game it's over
                     someoneWin = gameController.checkWinner(board);
                     isDraw = gameController.checkDraw(board);
-
                     if (someoneWin === true || isDraw === true) {
                         showWinner();
                         break
@@ -318,6 +340,7 @@ const screenController = ((gameController) => {
 
                     gameController.switchPlayer();
                     
+                    //Check if the opponent it's the IA, 
                     if(opponentOption === 2) {
                         symbol = gameController.getCurrentPlayer().symbol;
                         gameController.ComputerMove(board, symbol, difficultyNumber);
@@ -331,12 +354,8 @@ const screenController = ((gameController) => {
                         }
 
                         gameController.switchPlayer();    
-                    } else {
-
                     }
-
-                    
-                    
+                    //If the opponent it's another human, the loop start again
                 }                
             });
         });
@@ -346,230 +365,3 @@ const screenController = ((gameController) => {
 })(gameController());
 
 screenController.playRound();
-
-
-
-
-
-
-
-
-//Codigo que con IA sin minmax que funciona bien
-////Module
-// const gameBoard = (() => {
-
-//     const board = [
-//         ["", "", ""],
-//         ["", "", ""],
-//         ["", "", ""]
-//     ];
-
-//     const getBoard = () => board;
-
-//     const makeMove = (row, column, symbol) => {
-//         board[row][column] = symbol;
-//         return true
-//     };
-
-//     return { getBoard, makeMove }
-
-// })();
-
-// //Function Facotry
-// const createPlayer = (name, symbol) => {
-//     return { name, symbol };
-// };
-
-// //Module
-// function gameController() {
-//     const playerOne = createPlayer("playerOne", "X");
-//     const playerTwo = createPlayer("playerTwo", "O");
-//     let currentPlayer = playerOne;
-
-//     const switchPlayer = () => {
-//         currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
-//     }
-
-//     const getCurrentPlayer = () => currentPlayer;
-
-//     const ComputerMove = (board, symbol) => {
-//         let computerRow, computerCol;
-
-//         while (true) {
-//             computerRow = Math.floor(Math.random() * 3);
-//             computerCol = Math.floor(Math.random() * 3);
-
-//             if (board[computerRow][computerCol] === "") {
-//                 break;
-//             }
-//         }
-
-//         gameBoard.makeMove(computerRow, computerCol, symbol);
-//     }
-
-//     let round = 0;
-
-//     function checkWinner(board) {
-//         let gameDraw = null;
-//         round++
-
-//         const checkBoard = () => {
-//             // Check rows for a winner
-//             for (let i = 0; i < 3; i++) {
-//                 if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== '') {
-//                     gameDraw = false;
-//                 }
-//             }
-
-//             // Check columns for a winner
-//             for (let i = 0; i < 3; i++) {
-//                 if (board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] !== '') {
-//                     gameDraw = false;
-//                 }
-//             }
-
-//             // Check diagonals for a winner
-//             if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== '') {
-//                 gameDraw = false;
-//             }
-
-//             if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[0][2] !== '') {
-//                 gameDraw = false;
-//             }
-
-//             return gameDraw
-//         }
-
-//         const checkAllDirections = checkBoard();
-        
-
-//         if ((round >= 9) && (checkAllDirections === null)) {
-//             gameDraw = true;
-//         }
-
-//         return gameDraw;
-//     }
-
-//     const restartBoard = () => {
-//         const board = gameBoard.getBoard();
-//         currentPlayer = playerOne;
-//         for (let i = 0; i < 3; i++) {
-//             for (let j = 0; j < 3; j++) {
-//                 board[i][j] = "";
-//             }
-//         }
-//         round = 0;
-//     }
-
-//     return { getCurrentPlayer, switchPlayer, checkWinner, restartBoard, ComputerMove };
-// };
-
-
-// //Module
-// const screenController = ((gameController) => {
-
-//     const restartBtn = document.getElementById("restart-btn");
-//     restartBtn.addEventListener("click", () => {
-//         gameController.restartBoard();
-//         updateBoardUI();
-//     });
-
-//     const showDifficultyBtn = document.getElementById("difficulty-btn");
-//     const difficultyBtnContainer = document.querySelector(".choose-difficulty");
-//     showDifficultyBtn.addEventListener("click", () => {
-//         difficultyBtnContainer.classList.toggle("showed");
-//     });
-
-//     const showWinner = () => {
-//         const blur = document.querySelector(".blur");
-//         const showWinnerContainer = blur.querySelector(".show-winner");
-//         const winnerTitle = blur.querySelector(".winner-title");
-//         const winnerSymbol = blur.querySelector(".winner-symbol");
-
-//         blur.classList.add("display");
-//         showWinnerContainer.classList.add("active");
-
-//         blur.addEventListener("click", () => {
-//             blur.classList.remove("display");
-//             showWinnerContainer.classList.remove("active");
-
-//             gameController.restartBoard(board);
-            
-//             updateBoardUI();
-//         });
-
-//         const checkResult = gameController.checkWinner(gameBoard.getBoard());
-
-//         if (checkResult === false) {
-//             winnerTitle.textContent = "The winner is";
-//             winnerSymbol.textContent = gameController.getCurrentPlayer().symbol;
-//         } else if (checkResult === true) {
-//             winnerTitle.textContent = "It's a draw!";
-//             winnerSymbol.textContent = "X O";
-//         }
-//     }
-
-//     const updateBoardUI = () => {
-//         const board = gameBoard.getBoard();
-//         const allCells = document.querySelectorAll(".cell");
-
-//         allCells.forEach((cell, index) => {
-//             const row = cell.getAttribute("data-row");
-//             const col = cell.getAttribute("data-col");
-//             cell.textContent = board[row][col];
-//         });
-//     };
-
-//     const playRound = () => {
-//         const board = gameBoard.getBoard();
-
-//         let rowIndex, colIndex;
-
-//         const allCells = document.querySelectorAll(".cell");
-
-//         allCells.forEach(cell => {
-//             cell.addEventListener("click", () => {
-//                 let checkResult;
-
-//                 while (true) {
-                    
-//                     rowIndex = cell.getAttribute("data-row");
-//                     colIndex = cell.getAttribute("data-col");
-    
-//                     let symbol = gameController.getCurrentPlayer().symbol;
-    
-//                     if (board[rowIndex][colIndex] !== "") {
-//                         return
-//                     }
-    
-//                     gameBoard.makeMove(rowIndex, colIndex, symbol);
-//                     updateBoardUI();
-//                     checkResult = gameController.checkWinner(board);
-
-//                     if (checkResult === true || checkResult === false) {
-//                         showWinner();
-//                         break
-//                     }
-
-//                     gameController.switchPlayer();
-//                     symbol = gameController.getCurrentPlayer().symbol;
-//                     gameController.ComputerMove(board, "O");
-//                     updateBoardUI();
-//                     checkResult = gameController.checkWinner(board);
-
-//                     if (checkResult === true || checkResult === false) {
-//                         showWinner();
-//                         break
-//                     }
-
-//                     gameController.switchPlayer();
-                    
-//                 }                
-//             });
-//         });
-//     };
-
-//     return { playRound }
-// })(gameController());
-
-// screenController.playRound();
